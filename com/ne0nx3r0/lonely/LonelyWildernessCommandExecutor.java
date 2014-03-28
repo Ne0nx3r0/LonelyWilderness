77cabb9b34791e48a10d91bfb819ff8b1f901195
Ne0nx3r0 <Ne0nx3r0@viasmooth>
2014-03-28 16:25:48 -0500

    Added cooldown ( Fixes #1 ), set starting seed to begin at 255

--- com/ne0nx3r0/lonely/LonelyWildernessCommandExecutor.java
+++ com/ne0nx3r0/lonely/LonelyWildernessCommandExecutor.java
@@ -3,6 +3,7 @@ package com.ne0nx3r0.lonely;
 import java.util.Random;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
@@ -16,11 +17,25 @@ class LonelyWildernessCommandExecutor implements CommandExecutor {
     public LonelyWildernessCommandExecutor(LonelyWilderness plugin) {
         this.plugin = plugin;
     }
+    
+    long COOLDOWN;
 
     @Override
     public boolean onCommand(CommandSender cs, Command cmnd, String label, String[] args) {
         if(!(cs instanceof Player)) {
             cs.sendMessage(ChatColor.RED+"/wild cannot be used from console");
+        
+            return true;
+        }
+        
+        long now = System.currentTimeMillis();
+        
+        if(this.COOLDOWN < now) {
+            cs.sendMessage(ChatColor.RED+"/wild has a global cooldown for all users");
+            
+            int seconds = (int) ((now - this.COOLDOWN) / 1000);
+            
+            cs.sendMessage(ChatColor.RED+"You can try again in "+seconds+" seconds.");
         
             return true;
         }
@@ -46,9 +61,10 @@ class LonelyWildernessCommandExecutor implements CommandExecutor {
         
         cs.sendMessage(ChatColor.GREEN+"Sending you to a random location in the world!");
         
+        this.COOLDOWN = System.currentTimeMillis()+10000;
+        
         return true;
     }
-    
     
     public Location getRandomLocation(Player player, int minDistance,int maxDistance) {
         Random random = new Random();
@@ -56,18 +72,18 @@ class LonelyWildernessCommandExecutor implements CommandExecutor {
         Location lSeed = player.getLocation();
         
         Block bSendPlayerTo = lSeed.add(
-            random.nextInt(maxDistance*2-minDistance*2)-minDistance-maxDistance, 
-            255-lSeed.getBlockY(), 
-            random.nextInt(maxDistance*2-minDistance*2)-minDistance-maxDistance
+            random.nextInt((maxDistance-minDistance)*2)-minDistance-maxDistance, 
+            255, 
+            random.nextInt((maxDistance-minDistance)*2)-minDistance-maxDistance
         ).getBlock();
-        
-        while(bSendPlayerTo.getType().equals(org.bukkit.Material.AIR) || bSendPlayerTo.getType().equals(org.bukkit.Material.WATER)) {
-            if(bSendPlayerTo.getY() == 1 || bSendPlayerTo.getType().equals(org.bukkit.Material.WATER)) {
+
+        while(bSendPlayerTo.getType().equals(Material.AIR) || bSendPlayerTo.getType().equals(Material.WATER)) {
+            if(bSendPlayerTo.getY() == 1 || bSendPlayerTo.getType().equals(Material.WATER)) {
                 // reached the bottom or water, grab a new random location
                 bSendPlayerTo = lSeed.add(
-                    random.nextInt(maxDistance*2-minDistance*2)-minDistance-maxDistance, 
-                    255-lSeed.getBlockY(), 
-                    random.nextInt(maxDistance*2-minDistance*2)-minDistance-maxDistance
+                    random.nextInt((maxDistance-minDistance)*2)-minDistance-maxDistance, 
+                    255, 
+                    random.nextInt((maxDistance-minDistance)*2)-minDistance-maxDistance
                 ).getBlock();
             }
             else {
